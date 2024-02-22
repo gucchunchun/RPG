2ba50b15bdd7616a8c409653481615503ce5998d
gucchunchun <yunataniguchi23@gmail.com>
2024-02-23 01:22:39 +0900

    #2 Add Collusion Control on Player to Map
    *Add Collusion map\n*Add Func to check rectangular collision

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
+// CANVAS
 const CANVAS = document.getElementById('canvas');
 const C = CANVAS.getContext('2d');
-
 CANVAS.width = 1024;
 CANVAS.height = 576;
-
 C.fillStyle = '#FFFFFF';
 C.fillRect(0, 0, canvas.width, canvas.height);
 
-const IMAGE_MAP = new Image();
-IMAGE_MAP.src = './img/map/map.png';
+// COLLISION MAP (COLLISION: ./data/collision.js)
+const COLLISION_MAP = [];
+for(let i = 0; i < COLLISION.length; i += 70) {
+  COLLISION_MAP.push(COLLISION.slice(i, i + 70));
+}
 
-const CHARACTER_GIRL = new Image();
-CHARACTER_GIRL.src = './img/character/girlFront.png';
-
-class Sprite {
-  constructor({position, velocity, image}) {
-    this.position = position;
-    this.image = image;
-  }
-  draw() {
-    C.drawImage(this.image, this.position.x, this.position.y);
-  }
+// SETTING
+const OFFSET = {
+  x: -2330,
+  y: -1300,
 }
 const KEYS =  {
   ArrowUp: {
@@ -42,33 +37,172 @@ const KEYS =  {
   lastKey: undefined
 }
 
+class Boundary {
+  static width = 48 // 12*12(TILE) * 400 (ZOOM)
+  static height = 48 // 12*12(TILE) * 400 (ZOOM)
+  constructor({position}) {
+    this.position = position;
+    this.width = 48;
+    this.height = 48;
+  }
+  draw() {
+    // C.fillStyle =  '#FF0000';
+    C.fillStyle =  'transparent';
+    C.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update({position}) {
+    this.position.x = position.x;
+    this.position.y = position.y;
+  }
+}
+const BOUNDARIES = [];
+COLLISION_MAP.forEach((row, rowIndex)=>{
+  row.forEach((symbol, colIndex)=>{
+    if(symbol === 1025) {
+      BOUNDARIES.push(new Boundary({
+        position: {
+          x: colIndex * Boundary.width + OFFSET.x,
+          y: rowIndex * Boundary.height + OFFSET.y
+        }
+      }))
+    }
+  })
+})
+
+// Images
+const IMAGE_MAP = new Image();
+IMAGE_MAP.src = './img/map/map.png';
+const CHARACTER_GIRL = new Image();
+CHARACTER_GIRL.src = './img/character/girlFront.png';
+
+class Sprite {
+  constructor({position, velocity, image, frames = {max: 1}}) {
+    this.position = position;
+    this.image = image;
+    this.frames = frames;
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max;
+      this.height = this.image.height;
+    }
+  }
+  draw() {
+    C.drawImage(
+      this.image,
+      0,
+      0,
+      this.width,
+      this.height,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height,
+    );
+  }
+  update({position}) {
+    this.position.x = position.x;
+    this.position.y = position.y;
+  }
+}
+
 const BACK_GROUND = new Sprite({
   position: {
-    x: -2330,
-    y: -1300,
+    x: OFFSET.x,
+    y: OFFSET.y,
   },
   image: IMAGE_MAP
 });
+const CHARACTER = new Sprite({
+  position: {
+    x: CANVAS.width/2 - CHARACTER_GIRL.width/4/2,
+    y: CANVAS.height/2 - CHARACTER_GIRL.height/2,
+  },
+  image: CHARACTER_GIRL,
+  frames: {max: 4}
+});
+const LIST_MOVABLE = [BACK_GROUND, ...BOUNDARIES]
+
+function rectCollision({rect1, rect2}) {
+  return (
+    rect2.position.x <= rect1.position.x + rect1.width && 
+    rect1.position.x <= rect2.position.x + rect2.width &&
+    rect2.position.y <= rect1.position.y + rect1.height &&
+    rect1.position.y <= rect2.position.y + rect2.height
+  )
+}
 
 function animate() {
   window.requestAnimationFrame(animate);
   BACK_GROUND.draw();
-  C.drawImage(
-    CHARACTER_GIRL,
-    0,
-    0,
-    CHARACTER_GIRL.width/4,
-    CHARACTER_GIRL.height,
-    CANVAS.width/2 - CHARACTER_GIRL.width/8,
-    CANVAS.height/2 - CHARACTER_GIRL.height/2,
-    CHARACTER_GIRL.width/4,
-    CHARACTER_GIRL.height,
-  );
+  CHARACTER.draw();
+  BOUNDARIES.forEach(boundary=>{
+    boundary.draw();
+  })
 
-  if(KEYS.ArrowDown.pressed && KEYS.lastKey == KEYS.ArrowDown.name) BACK_GROUND.position.y = BACK_GROUND.position.y - 3;
-  else if(KEYS.ArrowUp.pressed && KEYS.lastKey == KEYS.ArrowUp.name) BACK_GROUND.position.y = BACK_GROUND.position.y + 3;
-  else if(KEYS.ArrowLeft.pressed && KEYS.lastKey == KEYS.ArrowLeft.name) BACK_GROUND.position.x = BACK_GROUND.position.x + 3;
-  else if(KEYS.ArrowRight.pressed && KEYS.lastKey == KEYS.ArrowRight.name) BACK_GROUND.position.x = BACK_GROUND.position.x - 3;
+  if(KEYS.ArrowDown.pressed && KEYS.lastKey == KEYS.ArrowDown.name) {
+    let canMove = true;
+    for(let i = 0; i < BOUNDARIES.length; i++) {
+      const BOUNDARY = BOUNDARIES[i];
+      if(rectCollision({rect1:CHARACTER, rect2:{...BOUNDARY, position: {x:BOUNDARY.position.x, y:BOUNDARY.position.y - 3}}})) {
+        canMove = false;
+        break;
+      }else {
+        continue;
+      }
+    }
+    if(canMove) {
+      LIST_MOVABLE.forEach((movable)=>{
+        movable.update({position: {x:movable.position.x, y:movable.position.y - 3}});
+      });
+    }
+  }else if(KEYS.ArrowUp.pressed && KEYS.lastKey == KEYS.ArrowUp.name) {
+    let canMove = true;
+    for(let i = 0; i < BOUNDARIES.length; i++) {
+      const BOUNDARY = BOUNDARIES[i];
+      if(rectCollision({rect1:CHARACTER, rect2:{...BOUNDARY, position: {x:BOUNDARY.position.x, y:BOUNDARY.position.y + 3}}})) {
+        canMove = false;
+        break;
+      }else {
+        continue;
+      }
+    }
+    if(canMove) {
+      LIST_MOVABLE.forEach((movable)=>{
+        movable.update({position: {x:movable.position.x, y:movable.position.y + 3}});
+      });
+    }
+  }else if(KEYS.ArrowLeft.pressed && KEYS.lastKey == KEYS.ArrowLeft.name) {
+    let canMove = true;
+    for(let i = 0; i < BOUNDARIES.length; i++) {
+      const BOUNDARY = BOUNDARIES[i];
+      if(rectCollision({rect1:CHARACTER, rect2:{...BOUNDARY, position: {x:BOUNDARY.position.x + 3, y:BOUNDARY.position.y}}})) {
+        canMove = false;
+        break;
+      }else {
+        continue;
+      }
+    }
+    if(canMove) {
+      LIST_MOVABLE.forEach((movable)=>{
+        movable.update({position: {x:movable.position.x + 3, y:movable.position.y}});
+      });
+    }
+  }else if(KEYS.ArrowRight.pressed && KEYS.lastKey == KEYS.ArrowRight.name) {
+    let canMove = true;
+    for(let i = 0; i < BOUNDARIES.length; i++) {
+      const BOUNDARY = BOUNDARIES[i];
+      if(rectCollision({rect1:CHARACTER, rect2:{...BOUNDARY, position: {x:BOUNDARY.position.x - 3, y:BOUNDARY.position.y}}})) {
+        canMove = false;
+        break;
+      }else {
+        continue;
+      }
+    }
+    if(canMove) {
+      LIST_MOVABLE.forEach((movable)=>{
+        movable.update({position: {x:movable.position.x - 3, y:movable.position.y}});
+      });
+    }
+  };
 }
 
 animate();
