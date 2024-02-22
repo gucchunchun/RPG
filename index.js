6f7a634183a900bcc4e788fc3bca153eb880c06a
gucchunchun <yunataniguchi23@gmail.com>
2024-02-22 14:32:36 +0900

    Project Setup
    Add HTML, JS, CSS and Canvas in HTML

---
+++ index.js
@@ -0,0 +1,9 @@
+const CANVAS = document.getElementById('canvas');
+const C = CANVAS.getContext('2d');
+
+CANVAS.width = 1024;
+CANVAS.height = 576;
+
+C.fillStyle = '#FFFFFF';
+C.fillRect(0, 0, canvas.width, canvas.height);
+
